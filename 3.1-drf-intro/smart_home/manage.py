b0bb67aa7f43e151972d41c8c7b39ecc7c764a2b
natalia.osnovenko <natalia.osnovenko@arixess.com>
2022-10-01 14:35:43 +0300

    added admin/ and api/ urls

--- 3.1-drf-intro/smart_home/manage.py
+++ 3.1-drf-intro/smart_home/manage.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+sys.setrecursionlimit(2000)
+
 
 def main():
     """Run administrative tasks."""
--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+
 from .views import SensorsList, SensorView, MeasurementDetail, MeasurementView
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('measurement.urls')),
     path('sensor/', SensorsList.as_view()),
     path('sensor/<pk>/', SensorView.as_view()),
     path('measurement/', MeasurementView.as_view()),
